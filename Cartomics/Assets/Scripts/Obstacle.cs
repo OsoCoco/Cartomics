767f6907947dca78a156e52df5763cd49653d21b
AngelOswaldo <aornelas@cocoschool.mx>
2021-04-09 13:29:19 -0500

    Obstaculos

---
+++ Cartomics/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Obstacle", menuName ="Obstacle")]
+public class Obstacle : ScriptableObject
+{
+    public Animator obsAnimator;
+    public string turnOnAction, turnOffAction;
+    public bool functionWithTime = false;
+    public bool ON = true;
+    public bool canActive = true;
+
+    [SerializeField]
+    private float delayForUse = 3.0f;
+    [SerializeField]
+    private float waitForActive = 1.5f;
+
+    public IEnumerator TurnOffObstacle()
+    {
+        ON = false;
+        canActive = false;
+        obsAnimator.SetTrigger(turnOffAction);
+        yield return new WaitForSeconds(delayForUse);
+        canActive = true;
+    }
+
+    public IEnumerator TurnOnObstacle()
+    {
+        ON = true;
+        canActive = false;
+        obsAnimator.SetTrigger(turnOnAction);
+        yield return new WaitForSeconds(delayForUse);
+        canActive = true;
+    }
+
+    public IEnumerator TurnOffObstacleByTime()
+    {
+        ON = false;
+        canActive = false;
+        obsAnimator.SetTrigger(turnOffAction);
+        yield return new WaitForSeconds(delayForUse);
+        ON = true;
+        obsAnimator.SetTrigger(turnOnAction);
+        yield return new WaitForSeconds(waitForActive);
+        canActive = true;
+    }
+
+}
---
+++ Cartomics/Assets/Scripts/ObstacleDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObstacleDisplay : MonoBehaviour, IPointerClickHandler
+{
+    public Obstacle myObstacle;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (myObstacle.functionWithTime == false)
+        {
+            if (myObstacle.canActive == true)
+            {
+                if (myObstacle.ON == true)
+                {
+                    StartCoroutine(myObstacle.TurnOffObstacle());
+                }
+                else if (myObstacle.ON == false)
+                {
+                    StartCoroutine(myObstacle.TurnOnObstacle());
+                }
+            }
+        }
+        else if (myObstacle.functionWithTime == true)
+        {
+            if (myObstacle.canActive == true)
+            {
+                StartCoroutine(myObstacle.TurnOffObstacleByTime());
+            }
+
+        }
+    }
+}
