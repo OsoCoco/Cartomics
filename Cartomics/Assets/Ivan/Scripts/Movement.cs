22775d5b6d082bf8b31fba786e0f57adc111b9fa
OsoCoco <ccordoba@cocoschool.mx>
2021-04-11 00:58:39 -0500

    Ya pude entrar, ver si no se rompio nada

---
+++ Cartomics/Assets/Ivan/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour,IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
