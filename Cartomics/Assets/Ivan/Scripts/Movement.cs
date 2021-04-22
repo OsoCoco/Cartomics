6851fcc55d8017125c66a68bf68e7dccafd833c0
OsoCoco <ccordoba@cocoschool.mx>
2021-04-21 21:35:55 -0500

    Movimiento

--- Cartomics/Assets/Ivan/Scripts/Movement.cs
+++ Cartomics/Assets/Ivan/Scripts/Movement.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour,IPointerClickHandler
 {
+    
+    [SerializeField] NavMeshAgent agent;
     public void OnPointerClick(PointerEventData eventData)
     {
-       
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(eventData.position);
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
