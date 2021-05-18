f5c66b4d0dd303d664575a345177738327d03db0
OsoCoco <ccordoba@cocoschool.mx>
2021-05-18 09:48:38 -0500

    Detalles Finales

--- Cartomics/Assets/Scripts/Eventos.cs
+++ Cartomics/Assets/Scripts/Eventos.cs
@@ -6,13 +6,20 @@ public class Eventos : MonoBehaviour
 {
     //ASIGNAMOS EL COMPONENTE LEMMING
     private Lemmings myVictim;
+    public Mesh spikeDown;
+    MeshFilter meshRenderer;
 
+    private void Start()
+    {
+        meshRenderer = GetComponentInChildren<MeshFilter>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         //OBTENEMOS EL LEMMING  Y LO MATAMAMOS CON LA FUNCION DEAD Y DESCATIVAMOS EL COLLIDER PARA NO MATAR A OTROS LEMMINGS
         if (other.GetComponent<Lemmings>())
         {
-            Debug.Log("Perdiste un Lemming :C");
+            //Debug.Log("Perdiste un Lemming :C");
+            meshRenderer.sharedMesh = spikeDown;
             myVictim = other.GetComponent<Lemmings>();
             myVictim.Dead();
             gameObject.GetComponent<Collider>().enabled = false;
